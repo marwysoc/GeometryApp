1e4a2a03f2a6eb0069620c2bec57ae51acb4019e
marwysoc <marta.wysocka01@gmail.com>
2018-01-06 19:23:36 +0100

    Add askForParams() method to Hexagon class

--- src/main/java/pl/yameo/internship/assignment/Hexagon.java
+++ src/main/java/pl/yameo/internship/assignment/Hexagon.java
@@ -4,27 +4,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Hexagon implements Shape {
-    private Double side = 0.0;
+    private Double edge = 0.0;
 
-    public Hexagon(Double side) {
-        setSide(side);
+    public Hexagon(Double edge) {
+        setEdge(edge);
     }
 
     @Override
     public String getName() { return "Hexagon"; }
 
     @Override
-    public List<Double> listDimensions() { return Arrays.asList(side); }
+    public List<Double> listDimensions() { return Arrays.asList(edge); }
 
     @Override
     public Double calculateArea() {
-        return (3*side*side*Math.sqrt(3))/2;
+        return (3* edge * edge *Math.sqrt(3))/2;
     }
 
     @Override
-    public Double calculatePerimeter() { return 6*side; }
+    public Double calculatePerimeter() { return 6* edge; }
 
-    public void setSide(Double side){
-        this.side = side;
+    public static void askForParams() {
+        System.out.println("Please provide the edge length:");
+    }
+
+    public void setEdge(Double edge){
+        this.edge = edge;
     }
 }
